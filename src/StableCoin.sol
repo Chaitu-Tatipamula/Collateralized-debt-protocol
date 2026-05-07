05a0f616b91c55bf35b60a351777f2340573c75f
Chaitu-Tatipamula <107246959+Chaitu-Tatipamula@users.noreply.github.com>
2026-05-08 02:09:52 +0530

    feat: implement StableCoin (DSC) ERC20 token

---
+++ src/StableCoin.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.15;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StableCoin is ERC20Burnable, Ownable {
+    error StableCoin__AmountMustBeMoreThanZero();
+    error StableCoin__NotEnoghBalance();
+    error StableCoin__ZeroAddress();
+
+    constructor() ERC20("StableCoin", "SCOIN") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert StableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert StableCoin__NotEnoghBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert StableCoin__ZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert StableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
