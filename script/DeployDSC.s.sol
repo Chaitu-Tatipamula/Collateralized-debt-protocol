f8e59d6576f2b4823fe97a08c273acd28e5bab72
Chaitu-Tatipamula <107246959+Chaitu-Tatipamula@users.noreply.github.com>
2026-05-09 02:10:19 +0530

    feat: add deployment scripts and local testnet configurations

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,13 +4,32 @@ pragma solidity ^0.8.15;
 import {Script} from "forge-std/Script.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 import {StableCoin} from "../src/StableCoin.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
-    function run() external returns(StableCoin, DSCEngine) {
-        vm.startBroadcast();
+
+    address[] public tokens;
+    address[] public priceFeeds;
+
+    function run() external returns (StableCoin, DSCEngine, HelperConfig) {
+
+        HelperConfig config = new HelperConfig();
+        (address wethUSDPriceFeed, 
+            address wbtcUSDPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+        tokens = [weth, wbtc];
+        priceFeeds = [wethUSDPriceFeed, wbtcUSDPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         StableCoin dsc = new StableCoin();
         // we should add tokens and price feeds from networkConfig
-        // DSCEngine engine = new DSCEngine(, dsc, )
+        DSCEngine engine = new DSCEngine(tokens, priceFeeds, address(dsc));
+        dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
+
+        return (dsc, engine, config);
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,27 +2,63 @@
 pragma solidity ^0.8.15;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
+
+    uint8 constant DECIMALS = 8;
+    int256 constant INITIAL_PRICE = 2000e8;
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     struct NetworkConfig{
         address wethUSDPriceFeed;
         address wbtcUSDPriceFeed;
         address weth;
         address wbtc;
+        uint256 deployerKey;
     }
 
-    NetworkConfig activeNetworkConfig;
+    NetworkConfig public activeNetworkConfig;
 
     constructor() {
-
+        if(block.chainid == 11155111){
+            activeNetworkConfig = getSepoliaNetworkConfig();
+        }else{
+            activeNetworkConfig = getOrCreateAnvilConfig();
+        }
     }
 
-    function getSepoliaNetworkConfig() public view returns (NetworkConfig config) {
+
+    function getSepoliaNetworkConfig() public view returns (NetworkConfig memory config) {
         config = NetworkConfig({
             wethUSDPriceFeed : 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             wbtcUSDPriceFeed : 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
-            weth : ,
-            wbtc : ,    
-        })
+            weth : 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9,
+            wbtc : 0x29f2D40B0605204364af54EC677bD022dA425d03,
+            deployerKey : vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory config) {
+        if(activeNetworkConfig.wethUSDPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockWethUSDPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        MockV3Aggregator mockWbtcUSDPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+
+        ERC20Mock weth = new ERC20Mock("Wrapped ETH", "WETH", msg.sender, 100e18);
+        ERC20Mock wbtc = new ERC20Mock("Wrapped BTC", "WBTC", msg.sender, 100e18);
+        vm.stopBroadcast();
+        
+        config = NetworkConfig({
+            wethUSDPriceFeed : address(mockWethUSDPriceFeed),
+            wbtcUSDPriceFeed : address(mockWbtcUSDPriceFeed),
+            weth : address(weth),
+            wbtc : address(wbtc),
+            deployerKey : DEFAULT_ANVIL_KEY
+        });
+        
     }
 }
